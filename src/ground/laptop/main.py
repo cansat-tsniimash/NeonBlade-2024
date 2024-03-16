fdc06cf85590bce3f30d982572cb2942e4e5b6ce
IB0370 <Ivanboz96@gmail.com>
2024-03-16 16:49:22 +0300

    Добавил кривых на график с ускорением

--- src/ground/laptop/main.py
+++ src/ground/laptop/main.py
@@ -17,8 +17,9 @@ class DataManager(QtCore.QObject):
     new_data = QtCore.pyqtSignal(list)
     mutex = QtCore.QMutex()
     #autoclose = QtCore.pyqtSignal(str)
-    #finished = QtCore.pyqtSignal()
-    
+    #finished = QtCore.pyqtSignal
+
+
     def run(self):
         self.mutex.lock()
         self.close_flag = 0
@@ -30,7 +31,7 @@ class DataManager(QtCore.QObject):
                 break
             self.mutex.unlock()
             h = math.sin(time.time())
-            self.new_data.emit([random.random(), time.time()])
+            self.new_data.emit([random.uniform(7, 10), time.time()])
             time.sleep(0.05)
 
     def finish(self):
@@ -54,18 +55,17 @@ class App(QWidget):
         self.data_thread.start()
         self.data_object.new_data.connect(self.new_data_reaction)
 
-        #
+
+
+        #График высоты
         self.graph_w = pg.GraphicsLayoutWidget()
         self.ui.verticalLayout_14.addWidget(self.graph_w)
         self.test_plot = self.graph_w.addPlot(1, 1, 1, 1)
-        
 
         self.curve  = self.test_plot.plot(np.array([[0, 0]]))
         self.new_curve = True
-
+        
         print(self.curve.getData())
-
-
 
         self.curve.setData(np.append(self.curve.getData()[0], 3), np.append(self.curve.getData()[1], 5))
         self.curve.setPen(color='r')
@@ -73,33 +73,39 @@ class App(QWidget):
         self.legend = self.test_plot.addLegend()
         self.legend.addItem(self.curve, 'test')
 
-
         axis_x = pg.AxisItem(orientation='bottom')
         axis_x.setLabel("ОСЬ_X")
         axis_y = pg.AxisItem(orientation='left')
         axis_y.setLabel("ОСЬ_Y")
         self.test_plot.setAxisItems({'bottom': axis_x, 'left': axis_y})
-        
 
 
 
-        #
+        #График ускорения
         self.graph1 = pg.GraphicsLayoutWidget()
         self.ui.verticalLayout_16.addWidget(self.graph1)
         self.plot1 = self.graph1.addPlot(1, 1, 1, 1)
-        
 
-        self.curve1  = self.plot1.plot(np.array([[0, 0]]))
+        self.X_curve = self.plot1.plot(np.array([[0, 0]]))
+        self.Y_curve = self.plot1.plot(np.array([[0, 0]]))
+        self.Z_curve = self.plot1.plot(np.array([[0, 0]]))
         self.new_curve = True
 
-        print(self.curve1.getData())
+        print(self.X_curve.getData())
+        print(self.Y_curve.getData())
+        print(self.Z_curve.getData())
 
-        self.curve1.setData(np.append(self.curve1.getData()[0], 3), np.append(self.curve1.getData()[1], 5))
-        self.curve1.setPen(color='r')
+        self.X_curve.setData(np.append(self.X_curve.getData()[0], 3), np.append(self.X_curve.getData()[1], 5))
+        self.X_curve.setPen(color='r')
+        self.Y_curve.setData(np.append(self.Y_curve.getData()[0], 3), np.append(self.Y_curve.getData()[1], 5))
+        self.Y_curve.setPen(color='g')
+        self.Z_curve.setData(np.append(self.Y_curve.getData()[0], 3), np.append(self.Y_curve.getData()[1], 5))
+        self.Z_curve.setPen(color='b')
 
         self.legend = self.plot1.addLegend()
-        self.legend.addItem(self.curve1, 'X')
-
+        self.legend.addItem(self.X_curve, 'X')
+        self.legend.addItem(self.Y_curve, 'Y')
+        self.legend.addItem(self.Z_curve, 'Z')
 
         axis_x = pg.AxisItem(orientation='bottom')
         axis_x.setLabel("ОСЬ_X")
@@ -109,37 +115,10 @@ class App(QWidget):
         
 
 
-
-        #
-        self.graph2 = pg.GraphicsLayoutWidget()
-        self.ui.verticalLayout_8.addWidget(self.graph2)
-        self.plot2 = self.graph2.addPlot(1, 1, 1, 1)
-        
-
-        self.curve2  = self.plot2.plot(np.array([[0, 0]]))
-        self.new_curve = True
-
-        print(self.curve2.getData())
-
-        self.curve2.setData(np.append(self.curve2.getData()[0], 3), np.append(self.curve2.getData()[1], 5))
-        self.curve2.setPen(color='r')
-
-        self.legend = self.plot2.addLegend()
-        self.legend.addItem(self.curve2, 'test')
-
-
-        axis_x = pg.AxisItem(orientation='bottom')
-        axis_x.setLabel("ОСЬ_X")
-        axis_y = pg.AxisItem(orientation='left')
-        axis_y.setLabel("ОСЬ_Y")
-        self.plot2.setAxisItems({'bottom': axis_x, 'left': axis_y})
-
-
-        #
+        #График GPS
         self.graph3 = pg.GraphicsLayoutWidget()
         self.ui.verticalLayout_10.addWidget(self.graph3)
         self.plot3 = self.graph3.addPlot(1, 1, 1, 1)
-        
 
         self.curve3  = self.plot3.plot(np.array([[0, 0]]))
         self.new_curve = True
@@ -152,7 +131,6 @@ class App(QWidget):
         self.legend = self.plot3.addLegend()
         self.legend.addItem(self.curve3, 'test')
 
-
         axis_x = pg.AxisItem(orientation='bottom')
         axis_x.setLabel("ОСЬ_X")
         axis_y = pg.AxisItem(orientation='left')
@@ -161,14 +139,11 @@ class App(QWidget):
         
 
 
-
-
-        #
+        #График угловой скорости
         self.graph4 = pg.GraphicsLayoutWidget()
         self.ui.verticalLayout_15.addWidget(self.graph4)
         self.plot4 = self.graph4.addPlot(1, 1, 1, 1)
         
-
         self.curve4  = self.plot4.plot(np.array([[0, 0]]))
         self.new_curve = True
 
@@ -180,7 +155,6 @@ class App(QWidget):
         self.legend = self.plot4.addLegend()
         self.legend.addItem(self.curve4, 'test')
 
-
         axis_x = pg.AxisItem(orientation='bottom')
         axis_x.setLabel("ОСЬ_X")
         axis_y = pg.AxisItem(orientation='left')
@@ -189,13 +163,10 @@ class App(QWidget):
         
 
 
-
-
-        #
+        #График давления
         self.graph5 = pg.GraphicsLayoutWidget()
         self.ui.verticalLayout_13.addWidget(self.graph5)
         self.plot5 = self.graph5.addPlot(1, 1, 1, 1)
-        
 
         self.curve5  = self.plot5.plot(np.array([[0, 0]]))
         self.new_curve = True
@@ -208,7 +179,6 @@ class App(QWidget):
         self.legend = self.plot5.addLegend()
         self.legend.addItem(self.curve5, 'test')
 
-
         axis_x = pg.AxisItem(orientation='bottom')
         axis_x.setLabel("ОСЬ_X")
         axis_y = pg.AxisItem(orientation='left')
@@ -217,14 +187,11 @@ class App(QWidget):
         
 
 
-
-
-        #
+        #График магнитометра
         self.graph6 = pg.GraphicsLayoutWidget()
         self.ui.verticalLayout_18.addWidget(self.graph6)
         self.plot6 = self.graph6.addPlot(1, 1, 1, 1)
         
-
         self.curve6  = self.plot6.plot(np.array([[0, 0]]))
         self.new_curve = True
 
@@ -236,7 +203,6 @@ class App(QWidget):
         self.legend = self.plot6.addLegend()
         self.legend.addItem(self.curve6, 'test')
 
-
         axis_x = pg.AxisItem(orientation='bottom')
         axis_x.setLabel("ОСЬ_X")
         axis_y = pg.AxisItem(orientation='left')
@@ -245,42 +211,11 @@ class App(QWidget):
         
 
 
-
-
-        #
-        self.graph7 = pg.GraphicsLayoutWidget()
-        self.ui.verticalLayout_17.addWidget(self.graph7)
-        self.plot7 = self.graph7.addPlot(1, 1, 1, 1)
-        
-
-        self.curve7  = self.plot7.plot(np.array([[0, 0]]))
-        self.new_curve = True
-
-        print(self.curve7.getData())
-
-        self.curve7.setData(np.append(self.curve7.getData()[0], 3), np.append(self.curve7.getData()[1], 5))
-        self.curve7.setPen(color='r')
-
-        self.legend = self.plot7.addLegend()
-        self.legend.addItem(self.curve7, 'test')
-
-
-        axis_x = pg.AxisItem(orientation='bottom')
-        axis_x.setLabel("ОСЬ_X")
-        axis_y = pg.AxisItem(orientation='left')
-        axis_y.setLabel("ОСЬ_Y")
-        self.plot7.setAxisItems({'bottom': axis_x, 'left': axis_y})
-        
-
-
-
-
-        #
+        #График температуры
         self.graph8 = pg.GraphicsLayoutWidget()
         self.ui.verticalLayout_19.addWidget(self.graph8)
         self.plot8 = self.graph8.addPlot(1, 1, 1, 1)
         
-
         self.curve8  = self.plot8.plot(np.array([[0, 0]]))
         self.new_curve = True
 
@@ -292,7 +227,6 @@ class App(QWidget):
         self.legend = self.plot8.addLegend()
         self.legend.addItem(self.curve8, 'test')
 
-
         axis_x = pg.AxisItem(orientation='bottom')
         axis_x.setLabel("ОСЬ_X")
         axis_y = pg.AxisItem(orientation='left')
@@ -300,18 +234,16 @@ class App(QWidget):
         self.plot8.setAxisItems({'bottom': axis_x, 'left': axis_y})
         
 
-
-
-        
+    #Обновление графиков    
     def new_data_reaction(self, data):
         print(data)
         self.ui.label_61.setText("{:.2f}".format(data[0]))
         if (not self.new_curve):
-            add_data_to_plot(self.curve, data[1], data[0])
-            add_data_to_plot(self.curve1, data[1], data[0])
+            add_data_to_plot(self.curve, time.time(), random.uniform(3, 5))
+            add_data_to_plot(self.X_curve, time.time(), random.uniform(7, 10))
             self.ui.label_6.setText("{:.2f}".format(data[0]))
-            add_data_to_plot(self.curve2, data[1], data[0])
-            self.ui.label_48.setText("{:.2f}".format(data[0]))
+            add_data_to_plot(self.Y_curve, time.time(), random.uniform(7, 10))
+            add_data_to_plot(self.Z_curve, time.time(), random.uniform(7, 10))
             add_data_to_plot(self.curve3, data[1], data[0])
             self.ui.label_12.setText("{:.2f}".format(data[0]))
             add_data_to_plot(self.curve4, data[1], data[0])
@@ -320,19 +252,17 @@ class App(QWidget):
             self.ui.label_30.setText("{:.2f}".format(data[0]))
             add_data_to_plot(self.curve6, data[1], data[0])
             self.ui.label_57.setText("{:.2f}".format(data[0]))
-            add_data_to_plot(self.curve7, data[1], data[0])
-            self.ui.label_59.setText("{:.2f}".format(data[0]))
             add_data_to_plot(self.curve8, data[1], data[0])
             self.ui.label_61.setText("{:.2f}".format(data[0]))
         else:
-            self.curve.setData(np.array([[data[1], data[0]]]))
-            self.curve1.setData(np.array([[data[1], data[0]]]))
-            self.curve2.setData(np.array([[data[1], data[0]]]))
+            self.curve.setData(np.array([[time.time(), random.uniform(3, 5)]]))
+            self.X_curve.setData(np.array([[time.time(), random.uniform(7, 10)]]))
+            self.Y_curve.setData(np.array([[time.time(), random.uniform(7, 10)]]))
+            self.Z_curve.setData(np.array([[time.time(), random.uniform(7, 10)]]))
             self.curve3.setData(np.array([[data[1], data[0]]]))
             self.curve4.setData(np.array([[data[1], data[0]]]))
             self.curve5.setData(np.array([[data[1], data[0]]]))
             self.curve6.setData(np.array([[data[1], data[0]]]))
-            self.curve7.setData(np.array([[data[1], data[0]]]))
             self.curve8.setData(np.array([[data[1], data[0]]]))
             self.new_curve = False
         
